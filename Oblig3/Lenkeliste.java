6d5af1ebb49ba0da5c38a741ab78b3fc47a6dd19
juze117 <juzengler@gmail.com>
2020-02-21 23:43:21 +0100

    method protos

--- Oblig3/Lenkeliste.java
+++ Oblig3/Lenkeliste.java
@@ -1,3 +1,28 @@
 class Lenkeliste<T> implements Liste<T>{
 
+
+
+//Instansvariabler
+
+
+
+//Konstruktør
+
+
+
+//metoder
+
+public int stoerrelse(){}
+
+public void leggTil(int pos, T x){}
+
+public void leggTil(T x){}
+
+public void sett(int pos, T x){}
+
+public T hent(int pos){}
+
+public T fjern(int pos){}
+
+public T fjern(){}
 }
