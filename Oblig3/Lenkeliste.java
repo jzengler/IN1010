669027be297efabb65ac61edc9684a3be6369492
juze117 <juzengler@gmail.com>
2020-02-27 00:04:58 +0100

    basic functionality, failing exception test

--- Oblig3/Lenkeliste.java
+++ Oblig3/Lenkeliste.java
@@ -1,10 +1,19 @@
 class Lenkeliste<T> implements Liste<T>{
 
+  class Node{
+
+    Node neste = null;
+    T data;
+
+    Node(T x){
+       data = x;
+    }
+  }
 
 
 //Instansvariabler
-
-
+private Node start = null;
+public static int antallNoder = 0;
 
 //Konstruktør
 
@@ -12,17 +21,159 @@ class Lenkeliste<T> implements Liste<T>{
 
 //metoder
 
-public int stoerrelse(){}
+//done
+public int stoerrelse(){
+  Node p = start;
+  int n = 0;
 
-public void leggTil(int pos, T x){}
+  while( p != null){
+      n++;
+      p = p.neste;
+  }
 
-public void leggTil(T x){}
+  return n;
+}
 
-public void sett(int pos, T x){}
+//done
+public void leggTil(int pos, T x){
+  if (pos < 0 || pos >= antallNoder)
+    throw new UgyldigListeIndeks(pos);
 
-public T hent(int pos){}
+  //Oppretter ny node
+  Node nyNode = new Node(x);
 
-public T fjern(int pos){}
+  //oppretter p og setter til start som utgangspunkt for løkken.
+  Node p = start;
 
-public T fjern(){}
+
+  if(pos == 0){
+    nyNode.neste = start;
+    start = nyNode;
+  }
+  else{
+    // gå videre i lenken så lenge "p" peker på en node
+    for(int n = 0; n < pos-1; n++){
+      p = p.neste;
+    }
+      // legg til peker til objektet etter ønsket posisjon
+      nyNode.neste = p.neste;
+
+      // lagre den nye noden i neste-peker i angitt posisjonen
+      p.neste = nyNode;
+
+  }
+  //inkrementer antallNoder
+  antallNoder++;
+  return;
+}
+
+//done
+public void leggTil(T x){
+
+  //Oppretter ny node
+  Node nyNode = new Node(x);
+
+  //legg til som første node i lenken hvis ikke node eksisterer i start
+  if(start == null){
+    start = nyNode;
+  }
+  //legg node til i slutten av lenken
+  else{
+
+    //oppretter p og setter til start som utgangspunkt for løkken.
+    Node p = start;
+
+    // gå videre i lenken så lenge "neste" peker på en node
+    while(p.neste != null){
+      p = p.neste;
+    }
+
+    // lagre den nye noden i første tomme neste-peker, dvs enden av lenken
+    p.neste = nyNode;
+  }
+
+  //inkrementer antallNoder
+  antallNoder++;
+  return;
+}
+
+//done
+public void sett(int pos, T x){
+  if (pos < 0 || pos>=antallNoder)
+    throw new UgyldigListeIndeks(pos);
+
+  Node p = start;
+
+  for(int i = 0; i < pos; i++){
+    p = p.neste;
+  }
+
+  p.data = x;
+  return;
+}
+
+//done
+public T hent(int pos){
+  if (pos < 0 || pos >= antallNoder)
+    throw new UgyldigListeIndeks(pos);
+
+  Node p = start;
+
+  for(int i = 0; i < pos; i++){
+    p = p.neste;
+  }
+
+  return p.data;
+}
+
+//done
+public T fjern(int pos){
+  if (pos < 0 || pos >= antallNoder)
+    throw new UgyldigListeIndeks(pos);
+
+  Node p = start;
+  Node r = null;
+
+  if(pos == 0){
+    start = p.neste;
+  }
+  else{
+    for(int i = 0; i < pos-2; i++){
+      p = p.neste;
+    }
+
+    r = p.neste;
+    if(r.neste != null){
+      p.neste = r.neste;
+      p = p.neste;
+    }
+    else{
+      p.neste = null;
+    }
+  }
+
+  //dekrementer antallNoder
+  antallNoder--;
+  return p.data;
+}
+
+//done
+public T fjern(){
+  if (antallNoder == 0){
+    throw new UgyldigListeIndeks(antallNoder);
+  }
+
+
+  //mellomlagrer første node
+  Node p = start;
+
+  // overskriver start med andre node i lenken slik at første tas ut
+  start = p.neste;
+
+  //dekrementer antall noder
+  antallNoder--;
+
+  // returnerer den første noden
+  return p.data;
+}
 }
---
+++ Oblig3/scratch.java
@@ -0,0 +1,29 @@
+class scratch{
+
+  public static void main(String[] args) {
+
+    System.out.println("Test tom liste");
+    Liste<String> liste = new Lenkeliste<String>();
+    liste.fjern();
+/*
+    liste.leggTil("Index 0");
+    liste.leggTil("Index 1");
+    liste.leggTil("Index 4");
+    liste.leggTil(2, "Index 2");
+    liste.leggTil(3, "Index 3");
+
+
+    System.out.println(liste.fjern());
+    System.out.println(liste.stoerrelse());
+    System.out.println(liste.fjern());
+    System.out.println(liste.stoerrelse());
+    System.out.println(liste.fjern());
+    System.out.println(liste.stoerrelse());
+    System.out.println(liste.fjern(1));
+    System.out.println(liste.stoerrelse());
+    System.out.println(liste.fjern(0));
+
+*/
+
+  }
+}
