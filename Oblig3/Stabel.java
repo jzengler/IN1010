234feb1d3a9ac96cec3834686461f2001e398c39
juze117 <juzengler@gmail.com>
2020-02-27 15:11:10 +0100

    comments

--- Oblig3/Stabel.java
+++ Oblig3/Stabel.java
@@ -5,13 +5,16 @@ class Stabel<T> extends Lenkeliste<T>{
 
   //push
   public void leggPaa(T x){
+    //legger til node på slutten av stacken
     leggTil(x);
     return;
   }
 
   //pop
   public T taAv(){
+    //hent størrelse på stacken og angi indeks
     int n = stoerrelse() - 1;
+    // fjern siste element og returner innhold
     return fjern(n);
   }
 }
