87c857be77acce16478794e3d6a78d147f9f814c
juze117 <juzengler@gmail.com>
2020-06-04 15:23:38 +0200

    proto Sted

--- Eksamen/src/Sted.java
+++ Eksamen/src/Sted.java
@@ -1,15 +1,29 @@
-class {
+class Sted{
     // KLASSEVARIABLER
 
 
 
     // INSTANSVARIABLER
-
+    String beskrivelse;
+    private Skattkiste kiste;
 
 
     // KONSTRUKTOER
-
+    Sted(String beskrivelse){
+        this.beskrivelse = beskrivelse;
+    }
 
 
     // METODER
+    void leggTilSkattkiste(Skattkiste kiste){
+        this.kiste = kiste;
+    }
+
+    Skattkiste hentSkattkiste(){
+        return kiste;
+    }
+
+    Sted gaaVidere(Sted sted){
+        return sted;
+    }
 }
